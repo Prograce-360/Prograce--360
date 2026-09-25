70f020a77b1623e2eeea68b3d4891768d945d88d
Prograce-360 <prograce.service@gmail.com>
2026-09-25 21:11:00 +0200

    Modify robots rules and sitemap URL
    Updated sitemap URL and adjusted rules format.

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,10 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {userAgent: "*",allow: "/",disallow: ["/admin/","/mon-espace/"]},
-    sitemap: "https://programme-360.vercel.app/sitemap.xml"
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://prograce.cd/sitemap.xml",
   };
 }
